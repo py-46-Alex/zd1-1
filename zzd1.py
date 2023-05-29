40974f07355289371415aea4d9f36b89e70b4349
py-46-Alex <90619667+py-46-Alex@users.noreply.github.com>
2023-05-29 18:54:55 +0300

    Update zzd1.py
    3aDaHue 1-1 TeCToBoe

--- zzd1.py
+++ zzd1.py
@@ -1,8 +1,6 @@
 import collections
 import asyncio
 import csv
-from datetime import datetime
-
 
 def listmaker():
     """создаст лист из айдишников
@@ -20,45 +18,17 @@ def listmaker():
 
 async def find_duplicates(lst):
     """на вход принимает лист из наших идишников.
-    На выход отдаст только те айдишники  которые повторялись в списке 3 раза
-    асинхронна. чтобы видеть глазами нужо евейтить"""
+    На выход отдаст только те айдишники  которые повторялись в списке 3 раза.
+    асинхронна."""
     counter = collections.Counter(lst)
-    # print(type(counter))
     duplicates = [id for id in counter if counter[id] == 3]
     return duplicates
-
-async def find_elses(lst):
-    counter = collections.Counter(lst)
-    elses = {}
-    todict = dict(counter)
-    for id, frequency in todict.items():
-        if frequency != 3:
-            if frequency not in elses:
-                elses.setdefault(frequency, [])
-                elses[frequency].append(id)
-            elif frequency in elses:
-                elses[frequency].append(id)
-    # return elses.items()
-    finish_view = {}
-    for frequency, ids in elses.items():
-        finish_view[frequency] = len(ids)
-    return finish_view.items()
-
 
 my_list = listmaker()
 
 async def main():
     duplicates = await find_duplicates(my_list)
-    print("Айдишники, повторяющиеся три раза: ", len(duplicates))
-    frequency = await find_elses(my_list)
-    for fr, kol in frequency:
-        print(f'вот такое количество уникальных айдишников {kol} светилось в файле с частотой {fr}')
-
-    print('________________________________________________________________________________________________________')
+    print("полный список уникальных айдишников которые повторялись только 3 раза: ", duplicates)
 
 if __name__ == '__main__':
-    # start = datetime.now()
     asyncio.run(main())
-    # print('текущее время работы программы')
-    # print(datetime.now() - start)
-    print('________________________________________________________________________________________________________')
